9e371e1b73317e29d60a9f516a8bf4675af5bdf4
Badman <arthur.georget@mailoo.org>
2016-01-18 14:00:21 +0100

    incremental video tree

--- playerVideo.js
+++ playerVideo.js
@@ -1,4 +1,5 @@
-var myVideo = document.getElementById("IDvideo"); 
+var myVideo = document.getElementById("IDvideo");
+var myVideoName = "0";
 
 function playPause() { 
     if (myVideo.paused) 
@@ -18,11 +19,22 @@ function makeSmall() {
 function makeNormal() { 
         myVideo.width = 420; 
 }
+
+function choixGauche() {
+        myVideoName = myVideoName + "0";
+        console.log(myVideoName);
+}
+
+function choixDroite() {
+        myVideoName = myVideoName + "1";
+        console.log(myVideoName);
+}
 //  Charge une autre vidéo dans le player
 function getVideo() {
-        var fileURL = document.getElementById("videoFile").value;  // get input field      
+        var fileURL = document.getElementById("videoFile").value;  // get input field
+        var myVideoURL = "../content/"+myVideoName+".mp4";
         if (fileURL != ""){
-            myVideo.src = fileURL;
+            myVideo.src = myVideoURL;
             myVideo.load();  // if HTML source element is used
             document.getElementById("play").click();  // start play
         } 
